8c0374fc0ac8b3a51f7433802d34e7896b79cbc0
jsipprelle <54649856+jsipprelle@users.noreply.github.com>
2021-06-06 20:16:39 -0700

    overhaul

--- script.js
+++ script.js
@@ -1,74 +1,79 @@
-function firstAnimation()
-{
-	var header1 = document.getElementsByTagName("h1")[0];
-	var header2 = document.getElementsByTagName("h2")[0];
-	header1.innerHTML = header1.textContent.replace(/\S/g, "<span class='letterTop' style='opacity: 0;'>$&</span>");
-	header2.innerHTML = header2.textContent.replace(/\S/g, "<span class='letterBot' style='opacity: 0;'>$&</span>");
-	header1.style.display = "inline";
-	header2.style.display = "inline";
-	
-	animateText();
+function projectsButton() {
+  if(document.getElementById("dropdownmenu").style.display != "flex") {
+	document.getElementById("dropdownmenu").style.display = "flex";
+  } else {
+	document.getElementById("dropdownmenu").style.display = "none";
+  }
 }
 
-function animateText()
-{
-	anime.timeline({loop: false}).add({
-		targets: '.animationTop .letterTop',
-		opacity: [0, 1],
-		easing: "linear",
-		duration: 1000,
-		delay: 300
-	});
-	
-	anime.timeline({loop: false}).add({
-		targets: '.animationBot .letterBot',
-		opacity: [0, 1],
-		easing: "linear",
-		duration: 1000,
-		delay: 1000
-	});
-}
+function load() {
+  var xmlhttp = new XMLHttpRequest();
 
-function loadHome()
-{
-	document.getElementById("projectsDiv").style.display = "none";
-	document.getElementById("resumeDiv").style.display = "none";
-	window.scrollTo(0, 0);
-	
-	var header1 = document.getElementsByTagName("h1")[0];
-	var header2 = document.getElementsByTagName("h2")[0];
-	header1.style.display = "none";
-	header2.style.display = "none";
-	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
-	
-	animateText();
-	
-	setTimeout(() =>
-	{
-		document.getElementById("indexDiv").style.display = "inline-block";
-		header1.style.display = "inline";
-		header2.style.display = "inline";
-	}, 400);
-}
-
-function loadResume()
-{
-	document.getElementById("indexDiv").style.display = "none";
-	document.getElementById("projectsDiv").style.display = "none";
-	window.scrollTo(0, 0);
-	
-	document.getElementById("resumeDiv").style.display = "inline-block";
-	
-	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
-}
-
-function loadProjects()
-{
-	document.getElementById("indexDiv").style.display = "none";
-	document.getElementById("resumeDiv").style.display = "none";
-	window.scrollTo(0, 0);
-	
-	document.getElementById("projectsDiv").style.display = "inline-block";
-	
-	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      json = JSON.parse(this.responseText);
+      
+      var projects_by_language = new Map();
+      var i;
+      for(i = 0; i < json.length; i++) {
+		var language = json[i].Language;
+		if(!projects_by_language.has(language)) {
+		  projects_by_language.set(language, []);
+		}
+		
+		projects_by_language.get(language).push(json[i].Name);
+      }
+	  
+	  var html = "";
+	  var languages = projects_by_language.keys();
+	  var language;
+	  for(language of languages) {
+		html += "<div class=\"projectlanguage\">";
+		html += "<b>" + language + "</b><br>";
+		var projects_of_language = projects_by_language.get(language);
+		var project;
+		for(projectname of projects_of_language) {
+	      html += "<a href=\"?project=" + projectname + "\"><span class=\"projectname\">" + projectname + "</span></a><br>";
+		}
+	    html += "</div>";
+	  }
+	  document.getElementById("dropdownmenu").innerHTML += html;
+	  
+	  
+	  var urlParams = new URLSearchParams(window.location.search);
+      var projectname = urlParams.get('project');
+      if(!projectname) {
+        document.getElementById("descriptiondiv").style.display = "initial";
+      } else {
+          var project;
+          var i;
+          for(i = 0; i < json.length; i++) {
+		    if(json[i].Name == projectname) {
+		      project = json[i];
+		      break;
+    		}
+	      }
+	      if(project) {
+			var projectHTML = "";
+			
+			projectHTML += "<div class=\"projectcontainername\">" + projectname + "</div>";
+			if(project.Screenshot) {
+			  projectHTML += "<img src=\"" + project.Screenshot + "\" style=\"display: block;margin-left: auto;margin-right: auto;\" alt=\"Screenshot of \"" + projectname + "\"\">";
+			}
+			projectHTML += "<p><strong>Language:</strong> " + project.Language + "</p>";
+			if(project.Libraries) {
+			  projectHTML += "<p><strong>Libraries:</strong> " + project.Libraries  + " </p>";
+			}
+			projectHTML += "<div class=\"project_description\">" + project.Description + "</div>";
+			  
+    		document.getElementById("projectdiv").innerHTML = projectHTML;
+	      } else {
+		    document.getElementById("projectdiv").innerHTML = "Project not found";
+    	  }
+	      document.getElementById("projectdiv").style.display = "initial";
+      }
+    }
+  };
+  xmlhttp.open("GET", "projects.json", true);
+  xmlhttp.send();
 }
